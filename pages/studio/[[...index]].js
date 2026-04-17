2fa0d5db911e084ecc68cf1b136b12ff4cc451a6
gmcryptodaddy <eenjoyer@gmail.com>
2026-04-17 16:24:31 +0300

    Update [[...index]].js

--- pages/studio/[[...index]].js
+++ pages/studio/[[...index]].js
@@ -1,14 +1,8 @@
-/**
- * Sanity Studio embedded at /studio
- * Visit: https://your-vercel-url.vercel.app/studio
- */
-
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../sanity.config'
+
+export const dynamic = 'force-static'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
-
-// Disable static generation for studio
-export const getServerSideProps = () => ({ props: {} })
