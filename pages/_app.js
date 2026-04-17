f639e9930083aaa3d3288a87795216fb51c3cb8c
gmcryptodaddy <eenjoyer@gmail.com>
2026-04-17 20:51:43 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" type="image/png" href="/logo.png" />
-        <link rel="shortcut icon" href="/logo.png" />
-        <link rel="apple-touch-icon" href="/logo.png" />
+<link rel="icon" type="image/png" href="/logo-full.png" />
+<link rel="shortcut icon" href="/logo-full.png" />
+<link rel="apple-touch-icon" href="/logo-full.png" />
       </Head>
       <Component {...pageProps} />
     </>
