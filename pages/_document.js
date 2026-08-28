ee599b22ae217005dc5d3160416d6848abb71ae7
gmcryptodaddy <eenjoyer@gmail.com>
2026-08-28 12:18:15 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,33 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const GA_TRACKING_ID = 'G-T5564DJCJY'
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* Favicon */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+
+        {/* Google Analytics (gtag.js) */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
